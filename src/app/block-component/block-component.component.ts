b5627da98e60be983058e0137a37f11b3c920750
Farid AL BARAZI <farid.al.barazi@gmail.com>
2022-09-29 23:18:42 +0200

    first commit

---
+++ src/app/block-component/block-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-block-component',
+  templateUrl: './block-component.component.html',
+  styleUrls: ['./block-component.component.css']
+})
+export class BlockComponentComponent implements OnInit {
+
+  title = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
